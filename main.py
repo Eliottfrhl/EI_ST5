e8db2262ddf544276dc564e922b9e39b137d0366
Eliott <frohlyeliott@gmail.com>
2023-11-16 11:28:35 +0100

    Sufficient condition implemented

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 import zlib
 import base64
 import copy
+from time import sleep
 
 
 def parse_template(template):
@@ -54,26 +55,59 @@ def find_cycles(template):
     return cycles, G, parra
 
 
-def resetChecker(cycle, root):
-    transitions = root.findall('.//transition')
+def get_clock_name(text, value):
+    text = text.split(",")
+    for index in range(len(text)):
+        while text[index][0] == " ":
+            text[index] = text[index][1:]
+        while text[index][-1] == " ":
+            text[index] = text[index][:len(text[index])-1]
+    for assignment in text:
+        if value in assignment:
+            index = assignment.index(value[0])
+            if assignment[index-1] == ":":
+                index -= 1
+            return assignment[:index]
+
+
+def suffCond(cycle, root):
+    result = {}
     for transition in cycle[1]:
-        xml_trans = transitions[0]
-        lab = xml_trans.find("label").text
-        print(lab)
-    return False
+        xml_trans = root.findall(".//transition[@id='" + transition + "']")
+        assignments = xml_trans[0].findall(".//label[@kind='assignment']")
+        guards = xml_trans[0].findall(".//label[@kind='guard']")
+        for assignment in assignments:
+            if "=0" in assignment.text:
+                clock_name = get_clock_name(assignment.text, "=0")
+                if clock_name in result.keys():
+                    result[clock_name][0] = True
+                else:
+                    result[clock_name] = [True, False]
+        for guard in guards:
+            if ">" in guard.text:
+                clock_name = get_clock_name(guard.text, ">")
+                if clock_name in result.keys():
+                    result[clock_name][1] = True
+                else:
+                    result[clock_name] = [False, True]
+    if result == {}:
+        return False
+    else:
+        for name, booleans in result.items():
+            if booleans == [True, True]:
+                return True
+        return False
 
 
 def main():
     tree = ET.parse('train-gate.xml')
     root = tree.getroot()
     transitions = root.findall('.//transition')
-    print(transitions)
     templates = root.findall('.//template')
     results = {}
     for template in templates:
         template_name = template.find('name').text
         cycles, G, parra = find_cycles(template)
-        print(f"Cycles in {template_name}:")
         result = []
         for cycle in cycles:
             transitions = []
@@ -96,15 +130,49 @@ def main():
                         new_cycle = [cycle[0], cycle[1]
                                      [:idx]+[parralel[1-par_idx]]+cycle[1][idx+1:]]
                         result.append(new_cycle)
+
         results[template_name] = result
-    return results
+    results_zeno = {}
+    results_potential_zeno = {}
+    for name, template in results.items():
+        results_zeno[name] = []
+        results_potential_zeno[name] = []
+        for cycle in template:
+            if suffCond(cycle, root):
+                results_zeno[name].append(cycle)
+            else:
+                results_potential_zeno[name].append(cycle)
+
+    return results, results_zeno, results_potential_zeno
 
 
 """if __name__ == "__main__":
     main()
 """
 
-tree = ET.parse('train-gate.xml')
+
+"""tree = ET.parse('train-gate.xml')
 root = tree.getroot()
 
-resetChecker(["id3", "id1"], root)
+resetChecker(["id3", "id1"], root)"""
+"""string = "    x:=0   , y<=10    "
+print(string)
+clock = get_clock_name(string, "<")
+print(clock)"""
+
+"""tree = ET.parse('train-gate.xml')
+root = tree.getroot()
+
+cycle = [[], ["id7", "id6"]]
+
+boolean = suffCond(cycle, root)
+
+print(boolean)"""
+
+r, rz, rpz = main()
+print("All cycles found :")
+print(r)
+print("All cycles that verify the sufficient condition :")
+print(rz)
+print("All cycles that do not verify the sufficient condition :")
+print(rpz)
